3402a498429d2d13664cd1b140fdf3afb3fea742
dudamollica <dudarenno@yahoo.com.br>
2023-05-01 17:43:09 -0300

    Fix: port

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import app, { init } from '@/app';
 
-const port = +process.env.PORT || 5000;
+const port = +process.env.PORT || 4000;
 
 init().then(() => {
-  app.listen(4002, () => {
+  app.listen(port, () => {
     /* eslint-disable-next-line no-console */
     console.log(`Server is listening on port ${port}.`);
   });
